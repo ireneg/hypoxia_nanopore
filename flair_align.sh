e979297ba6895b71427cc0678f1c424d54293dc1
Irene Gallego Romero <igr@ntu.edu.sg>
2020-04-21 14:19:44 +1000

    Ready for batch submission testing.

--- flair_align.sh
+++ flair_align.sh
@@ -3,35 +3,27 @@
 #This array script makes a bam out of each read, and then makes a frequency table of read lengths.
 #After using this script, proceed to freqtables.R 
 
-# Partition for the job:
-#SBATCH --partition=mig
-
 # The name of the job:
-#SBATCH --job-name="1align"
-
-# The project ID which this job should run under:
-#SBATCH --account="punim0586"
+#SBATCH --job-name="hif_align"
 
 # Maximum number of tasks/CPU cores used by the job:
 #SBATCH --ntasks=1
 
 # The amount of memory in megabytes per process in the job:
-#SBATCH --mem-per-cpu=40000
+#SBATCH --mem-per-cpu=25000
 
 # Send yourself an email when the job:
 # aborts abnormally (fails)
 #SBATCH --mail-type=FAIL
-# begins
-#SBATCH --mail-type=BEGIN
 # ends successfully
 #SBATCH --mail-type=END
 
 # Use this email address:
-#SBATCH --mail-user=rkano@student.unimelb.edu.au
+#SBATCH --mail-user=irene.gallego@unimelb.edu.au
 # The maximum running time of the job in days-hours:mins:sec
 #SBATCH --time=1000
 
-#SBATCH --array=1
+#SBATCH --array=1-3
 
 # check that the script is launched with sbatch
 if [ "x$SLURM_JOB_ID" == "x" ]; then
@@ -39,17 +31,19 @@ if [ "x$SLURM_JOB_ID" == "x" ]; then
    exit 1
 fi
 
+# Assign the right ID to slurm task manager:
 
-# 0 - Boilerplate
-source ~/virtualenv/python2.7.13/bin/activate
+sampleID=`head -n $SLURM_ARRAY_TASK_ID /data/cephfs/punim0586/igallego/repos/demultiplexing_ids.txt | tail -n 1 | awk '{ print $1 }'`
 
-cd /data/cephfs/punim0586/rkano/VEGFA/1_VEGFA/flair/align
+# The actual command
 
-module load SAMtools
+module load Cython
 module load Python/2.7.13-spartan_gcc-6.2.0
+module load Pysam/0.15.1-GCC-6.2.0-Python-2.7.13
+
+source ~/virtualenv/python2.7.13/bin/activate
+module load SAMtools
 module load BEDTools
 module load minimap2
 
-
-# 1 - Flair align
-python /data/cephfs/punim0586/rkano/flair/flair.py align -g /data/cephfs/punim0586/shared/genomes/hg38/Homo_sapiens.GRCh38.dna.primary_assembly.fa -r /data/cephfs/punim0586/rkano/VEGFA/1_VEGFA/porechop/cat/BC${SLURM_ARRAY_TASK_ID}cat.fastq -o align.${SLURM_ARRAY_TASK_ID}
+python /data/cephfs/punim0586/shared/bin/flair/flair.py align -g /data/cephfs/punim0586/shared/genomes/hg38/hg38_renamed_no_alt_analysis_set.fa -r /data/cephfs/punim0586/shared/raw_data/epas_nanopore/hif1a/concatenated/$sampleID.fastq -o /data/cephfs/punim0586/igallego/hypoxia_nanopore/mapped/hif1a/$sampleID.mapped
